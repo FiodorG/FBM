6ff6fd8c173389d5dcce9c13b730a22e04e8d58b
FiodorG <fedor.gorokhovik@gmail.com>
2018-05-06 18:33:11 -0400

    adding the weedend's work

--- lib/main_distributed.py
+++ lib/main_distributed.py
@@ -1,10 +1,9 @@
 import time
 import numpy as np
-import matplotlib.pyplot as plt
 import multiprocessing as mp
-import scipy.integrate as integrate
 import itertools
-from utilities import index_of_closest, eigenvalues, drift, fbm_path
+from utilities import eigenvalues, drift, fbm_path
+from utilities import plot_pdf, plot_cdf, plot_G0, plot_G1, plot_G2Mu, plot_G2Nu
 
 
 def func(npaths, N, H, mu, nu, m):
@@ -16,7 +15,7 @@ def func(npaths, N, H, mu, nu, m):
     N: lattice size
     H: Hurst coefficient
     mu: deterministic drift
-    nu: fbm-related drift
+    nu: fbm-specific drift
     m: values to find stopping times of
     """
 
@@ -28,38 +27,53 @@ def func(npaths, N, H, mu, nu, m):
     for i in range(0, npaths):
         X = fbm_path(N, H, lambdas, dr)
 
-        X_max = np.maximum.accumulate(X)
+        X_max = np.maximum(np.maximum.accumulate(X), 0)  # starting point missing
         stopping_times[i, :] = np.searchsorted(X_max, m)
 
     return stopping_times
 
 
-def main():
-    """
-    For a diffusion of the form z_t = x_t + mu*t + nu*t**(2H)
-    """
+def main(npaths, N, H, mu, nu, m, njobs = -1):
 
-    npaths = 100000    # Numbers of MC paths
-    N = 2**12          # Size of the lattice
-    H = 0.55           # Hurst exponent
+    if njobs == -1:
+        njobs = mp.cpu_count()
 
-    mu = 0.1           # Drift
-    nu = 0.0           # Autocorrelation drift
+    args = itertools.repeat([int(npaths/njobs), N, H, mu, nu, m], njobs)
 
-    m = np.linspace(0.1, 2, num=20)  # stopping values to store
-
-    processes = mp.cpu_count()
-    args = itertools.repeat([int(npaths/processes), N, H, mu, nu, m], processes)
-
-    with mp.Pool(processes=processes) as pool:
+    with mp.Pool(processes=njobs) as pool:
         results = pool.starmap(func, args)
 
     return np.concatenate(results)
 
 
 if __name__ == '__main__':
+
+    """
+    For a diffusion of the form z_t = x_t + mu*t + nu*t**(2H)
+    """
+
+    npaths = 1000000   # Numbers of MC paths
+    N = 2**13          # Size of the lattice
+    H = 0.55           # Hurst exponent
+
+    mu = 0.0           # Drift
+    nu = 0.1           # Autocorrelation drift
+
+    m = np.linspace(0.1, 2, num=20)  # stopping values to store
+
     start = time.time()
-    results = main()
+    results = main(npaths, N, H, mu, nu, m)
     end = time.time()
 
-    print(end - start)
+    print('Run time: %.2f' % (end - start))
+
+    if False:
+        x = 0.8
+
+        plot_pdf(x, mu, nu, H, N, m, results)
+        plot_cdf(x, mu, nu, H, N, m, results)
+
+        plot_G0(x, mu, nu, H, N, m, results)
+        plot_G1(x, mu, nu, H, N, m, results)
+        plot_G2Mu(x, mu, nu, H, N, m, results)
+        plot_G2Nu(x, mu, nu, H, N, m, results)
--- lib/utilities.py
+++ lib/utilities.py
@@ -2,6 +2,8 @@ import numpy as np
 import mpmath
 import math
 import scipy
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
 
 np.random.seed(10000)
 
@@ -18,19 +20,24 @@ def gamma(k, H):
     return (abs(k - 1)**(2. * H) + abs(k + 1)**(2. * H) - 2. * abs(k)**(2. * H))
 
 
-def G(m, t, mu, nu, H, epsilon, N):
+def G(m, t, mu, nu, H, N):
+    epsilon = H - 0.5
     tau = 1. / N
     return \
-            math.exp(-N_func(m, t, mu, nu, H, epsilon, tau)) \
+            math.exp(-N_func(m, t, mu, nu, H, tau)) \
           * G0(m, t) \
-          * math.exp(2. * epsilon * (G1(m, t, tau) - mu * G2Mu(m, t, tau) - nu * G2Nu(m, t, tau)) / G0(m, t))
+          * math.exp(2. * epsilon * (G1(m, t) - mu * G2Mu(m, t, tau) - nu * G2Nu(m, t, tau)) / G0(m, t))
 
 
 def G0(m, t):
     return m / (2. * np.sqrt(math.pi) * t**1.5) * math.exp(- m * m / 4. / t)
 
 
-def G1(m, t, tau):
+def R0Plus(z):
+    return z* math.exp(- z * z / 2.)
+
+
+def G1(m, t):
     z = m / math.sqrt(2. * t)
 
     return \
@@ -38,6 +45,17 @@ def G1(m, t, tau):
         (
             I_func(z)
           + (z * z - 1.) * math.log(t)
+          + z * z * (math.log(2. * z * z) + np.euler_gamma)
+          - 4. * math.log(z)
+          - 4. * np.euler_gamma
+        )
+
+def W(z):
+
+    return \
+        0.5 * G0(m, t) * \
+        (
+            I_func(z)
           + z * z * (math.log(2. * z * z) + np.euler_gamma)
           - 4. * math.log(z)
           - 4. * np.euler_gamma
@@ -67,7 +85,13 @@ def G2Mu(m, t, tau):
 
 
 def G2Nu(m, t, tau):
-    return 0.5 * (G2Beta(m, t, tau) - G2Alpha(m, t, tau))
+#    return 0.5 * (G2Beta(m, t, tau) - G2Alpha(m, t, tau))
+
+    z = m / math.sqrt(2. * t)
+
+    return \
+            math.exp(-z * z / 2.) * (I_func(z) - 2.) / (4. * math.sqrt(math.pi * t)) \
+          + math.exp(-z * z / 2.) * z * z * (math.log(2. * z * z) + np.euler_gamma) / (4. * math.sqrt(math.pi * t))
 
 
 def I_func(x):
@@ -79,7 +103,8 @@ def I_func(x):
           + 2.
 
 
-def N_func(m, t, mu, nu, H, epsilon, tau):
+def N_func(m, t, mu, nu, H, tau):
+    epsilon = H - 0.5
     D = 2. * H * tau**(2. * H - 1.)
 
     return m * 0.5 * (mu / D + nu) + \
@@ -88,17 +113,22 @@ def N_func(m, t, mu, nu, H, epsilon, tau):
 
 def eigenvalues(N, H):
     covariances = np.zeros(2 * N)
-    for k in range(0, N):
-        covariances[k] = gamma(k, H)
-    for k in range(N + 1, 2 * N):
-        covariances[k] = gamma(2 * N - k, H)
+    for i in range(0, N):
+        covariances[i] = gamma(i, H)
+    for i in range(N + 1, 2 * N):
+        covariances[i] = gamma(2 * N - i, H)
 
     return np.sqrt(np.real(np.fft.ifft(covariances)))
 
 
 def fbm_path(N, H, eigenvalues, drift):
+    """
+    Technically using different seeds do not guarantee
+    that numbers are going to be independant between
+    processes.
+    """
     np.random.seed()
-    V = np.random.normal(0, 1, 2 * N)
+    V = np.random.normal(loc=0, scale=1, size=2 * N)
     W = np.zeros(2 * N, complex)
 
     W[0] = V[0]
@@ -112,24 +142,179 @@ def fbm_path(N, H, eigenvalues, drift):
     return np.cumsum(Z[0:N]) / N**H + drift
 
 
-def fbm_noise(N, eigenvalues):
-    V = np.random.normal(0, 1, 2 * N)
-    W = np.zeros(2 * N, complex)
-
-    W[0] = V[0]
-    W[N] = V[N]
-
-    W[1:N] = (V[1:N] + V[N + 1:2 * N] * 1j) / np.sqrt(2.)
-    W2 = (V[1:N] - V[N + 1:2 * N] * 1j) / np.sqrt(2.)
-    W[N + 1:2 * N] = W2[::-1]
-
-    return np.real(np.fft.fft(eigenvalues * W))
-
-
 def drift(mu, nu, N, H):
-    return [mu * t + nu * t**(2. * H) for t in np.linspace(1. / N, 1., num=N)]
+    return [mu * t + nu * (t**(2. * H)) for t in np.linspace(1. / N, 1., num=N)]
 
 
 def empirical_cdf(array, t):
     return len(array[array < t]) / len(array)
 
+
+def integration_constant(x, mu, nu, H, N, m):
+    """
+    Integration constant to make G a density.
+    Computed numerically on some defined interval
+    """
+    return integrate.quad(lambda t: G(x, t, mu, nu, H, N), 0.005, np.inf)[0]
+
+
+def plot_pdf(x, mu, nu, H, N, m, results):
+    """
+    Plotting PDF of H = inf_{s}{ s | Z_s == x},
+    i.e. P(H == t) for t in (0, 1).
+    """
+    constant = integration_constant(x, mu, nu, H, N, m)
+    t_values = np.linspace(0.01, 0.99, num=50)
+
+    values_analytical = [G(x, t, mu, nu, H, N) / constant for t in t_values]
+    values_simulated = results[:, index_of_closest(m, x)] / float(N)
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.hist(values_simulated, bins=80, range=(0, 1), density=True)
+    ax.plot(t_values, values_analytical)
+    ax.set_xlim([0, 0.98])
+    ax.set_ylim([0, max(values_analytical) + 0.2])
+    plt.ylabel('PDF')
+    plt.xlabel('Time')
+    plt.show()
+
+
+def plot_cdf(x, mu, nu, H, N, m, results):
+    """
+    Plotting CDF of H = inf_{s}{ s | Z_s == x},
+    i.e. P(H == t) for t in (0, 1).
+    """
+    constant = integration_constant(x, mu, nu, H, N, m)
+    t_values = np.linspace(0.01, 0.99, num=50)
+
+    values_simulated = results[:, index_of_closest(m, x)] / float(N)
+
+    plt.figure()
+    plt.plot(t_values, [empirical_cdf(values_simulated, t) for t in t_values], 'o')
+    plt.plot(t_values, [integrate.quad(lambda t: G(x, t, mu, nu, H, N), 0.001, t)[0] / constant for t in t_values])
+    plt.show()
+
+
+def plot_G0(x, mu, nu, H, N, m, results):
+    """
+    H = inf_{s}{ s | Z_s == x}, i.e. we are interested in
+    P(H == t) for t in (0, 1).
+    Checks G0, historical vs simulated.
+    Assumes mu=nu=0, H=0.5
+    """
+    constant = integration_constant(x, mu, nu, H, N, m)
+
+    values_simulated = results[:, index_of_closest(m, x)] / float(N)
+    hist = np.histogram(values_simulated, bins=80, range=(np.min(values_simulated), 1), density=True)
+
+    G_values = hist[0][:-1]
+    G_bins = (hist[1][1:-1] + hist[1][:-2]) * 0.5
+
+    G0_simulated = G_values
+    G0_analytical = [G0(x, t) / constant for t in G_bins]
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.scatter(G_bins, G0_simulated, c='r', marker='+')
+    ax.plot(G_bins, G0_analytical)
+    plt.xlabel('Time')
+    plt.ylabel('G0')
+    ax.legend(['analytic', 'empirical'])
+    ax.set_title('G0(simu) vs G0')
+    ax.grid(True)
+    plt.show()
+
+
+def plot_G1(x, mu, nu, H, N, m, results):
+    """
+    H = inf_{s}{ s | Z_s == x}, i.e. we are interested in
+    P(H == t) for t in (0, 1).
+    Checks G1, historical vs simulated, knowing G0.
+    Assumes mu=nu=0.
+    """
+    constant = integration_constant(x, mu, nu, H, N, m)
+
+    values_simulated = results[:, index_of_closest(m, x)] / float(N)
+    hist = np.histogram(values_simulated, bins=50, range=(np.min(values_simulated), 1), density=True)
+
+    G_values = hist[0][:-1]
+    G_bins = (hist[1][1:-1] + hist[1][:-2]) * 0.5
+
+    G1_simulated = [math.log(y * constant / G0(x, t)) for y, t in zip(G_values, G_bins)]
+    G1_analytical = [2. * (H - 0.5) * G1(x, t) / G0(x, t) for t in G_bins]
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.scatter(G_bins, G1_simulated, c='r', marker='+')
+    ax.plot(G_bins, G1_analytical)
+    plt.xlabel('Time')
+    plt.ylabel('log(G(simu)/G0)')
+    ax.legend(['analytic', 'empirical'])
+    ax.set_title('log(G(simu)/G0) vs 2*Eps*G1/G0')
+    ax.grid(True)
+    plt.show()
+
+
+def plot_G2Mu(x, mu, nu, H, N, m, results):
+    """
+    H = inf_{s}{ s | Z_s == x}, i.e. we are interested in
+    P(H == t) for t in (0, 1).
+    Checks G2Mu, historical vs simulated, knowing G0, G1.
+    Assumes nu=0.
+    """
+    constant = integration_constant(x, mu, nu, H, N, m)
+
+    values_simulated = results[:, index_of_closest(m, x)] / float(N)
+    hist = np.histogram(values_simulated, bins=40, range=(np.min(values_simulated), 1), density=True)
+
+    G_values = hist[0][1:-1]
+    G_bins = (hist[1][2:-1] + hist[1][1:-2]) * 0.5
+
+    G2Mu_simulated = [
+        - ((math.log(y * math.exp(N_func(x, t, mu, nu, H, 1. / N)) * constant / G0(x, t))) / 2. / (H - 0.5) - G1(x, t) / G0(x, t)) / mu
+          for y, t in zip(G_values, G_bins) ]
+    G2Mu_analytical = [G2Mu(x, t, 1. / N) / G0(x, t) for t in G_bins]
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.scatter(G_bins, G2Mu_simulated, c='r', marker='+')
+    ax.plot(G_bins, G2Mu_analytical)
+    plt.xlabel('Time')
+    plt.ylabel('G2Mu/G0')
+    ax.legend(['analytic', 'empirical'])
+    ax.set_title('-((log(G(simu)/G0)+N)/(2*Eps) - G1/G0)/mu vs G2Mu/G0')
+    ax.grid(True)
+    plt.show()
+
+
+def plot_G2Nu(x, mu, nu, H, N, m, results):
+    """
+    H = inf_{s}{ s | Z_s == x}, i.e. we are interested in
+    P(H == t) for t in (0, 1).
+    Checks G2Nu, historical vs simulated, knowing G0, G1.
+    Assumes mu=0.
+    """
+    constant = integration_constant(x, mu, nu, H, N, m)
+
+    values_simulated = results[:, index_of_closest(m, x)] / float(N)
+    hist = np.histogram(values_simulated, bins=40, range=(np.min(values_simulated), 1), density=True)
+
+    G_values = hist[0][1:-1]
+    G_bins = (hist[1][2:-1] + hist[1][1:-2]) * 0.5
+
+    G2Mu_simulated = [
+        - ((math.log(y * math.exp(N_func(x, t, mu, nu, H, 1. / N)) * constant / G0(x, t))) / 2. / (H - 0.5) - G1(x, t) / G0(x, t)) / nu
+          for y, t in zip(G_values, G_bins) ]
+    G2Mu_analytical = [G2Nu(x, t, 1. / N) / G0(x, t) for t in G_bins]
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.scatter(G_bins, G2Mu_simulated, c='r', marker='+')
+    ax.plot(G_bins, G2Mu_analytical)
+    plt.xlabel('Time')
+    plt.ylabel('G2Nu/G0')
+    ax.legend(['analytic', 'empirical'])
+    ax.set_title('-((log(G(simu)/G0)+N)/(2*Eps) - G1/G0 - muG2Mu/G0)/nu vs G2Nu/G0')
+    ax.grid(True)
+    plt.show()
